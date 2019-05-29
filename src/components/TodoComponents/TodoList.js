c51e75077b352a616b509eb293defe2a6cd495ee
Noble Obioma <nobioma1@gmail.com>
2019-05-29 16:06:27 +0100

    Add delete button to TodoList and Todo, styling components and adding background image svg

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import Todo from './Todo';
+import DeleteButton from './DeleteButton';
+
 
 const TodoList = ({ todoList }) => {
   return (
     <div className="todo-list">
-      <h3>All Tasks</h3>
+      <div className="header">
+        <h3>All Tasks</h3>
+        <div className="header-buttons">
+          <DeleteButton text="Clear Completed" />
+          <DeleteButton text="Clear All" />
+        </div>
+      </div>
       <ol>
         {todoList.map(todo => (
           <li key={todo.id}>
