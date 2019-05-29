74f361a13df42118aff473c71e965779a999a7f0
Noble Obioma <nobioma1@gmail.com>
2019-05-29 16:10:36 +0100

    Add  event handlers on the form (onChange, onKeyPress) and value. on Click Event handler on the button

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { FaPlus } from 'react-icons/fa';
 
-const TodoForm = props => {
+const TodoForm = ({ inputHandler, addHandler, enterKeyHandler, text }) => {
   return (
     <div className="todo-input">
       <input
         placeholder="What do you want to do?"
+        onChange={event => inputHandler(event)}
+        onKeyPress={(event) => enterKeyHandler(event)}
+        value={text}
       />
-      <button><FaPlus /></button>
+      <button onClick={() => addHandler()}>
+        <FaPlus />
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default TodoForm;
