29cffdb412658dec5ed7a9cf01ca098cea517f8c
Noble Obioma <nobioma1@gmail.com>
2019-05-30 11:22:24 +0100

    TodoForm: add form tag to input, keypress passing in method enterKeyHander to return event object

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -4,15 +4,17 @@ import { FaPlus } from 'react-icons/fa';
 const TodoForm = ({ inputHandler, addHandler, enterKeyHandler, text }) => {
   return (
     <div className="todo-input">
-      <input
-        placeholder="What do you want to do?"
-        onChange={event => inputHandler(event)}
-        onKeyPress={(event) => enterKeyHandler(event)}
-        value={text}
-      />
-      <button onClick={() => addHandler()}>
-        <FaPlus />
-      </button>
+      <form>
+        <input
+          placeholder="What do you want to do?"
+          onChange={inputHandler}
+          onKeyPress={enterKeyHandler}
+          value={text}
+        />
+        <button onClick={addHandler}>
+          <FaPlus />
+        </button>
+      </form>
     </div>
   );
 };
