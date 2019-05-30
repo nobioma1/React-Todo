83d5f021e375c53a84107802e971f329e8c96e3d
Noble Obioma <nobioma1@gmail.com>
2019-05-30 12:33:16 +0100

    add methods as pass methods as props

--- src/App.js
+++ src/App.js
@@ -2,24 +2,26 @@ import React from 'react';
 import './components/TodoComponents/Todo.css';
 import TodoList from './components/TodoComponents/TodoList';
 import TodoForm from './components/TodoComponents/TodoForm';
+import Search from './components/TodoComponents/Search';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       todoList: [
-        {
-          task: 'Organize Garage',
-          id: 1528817077286,
-          completed: false
-        },
-        {
-          task: 'Bake Cookies',
-          id: 1528817084358,
-          completed: false
-        }
+        // {
+        //   task: 'Organize Garage',
+        //   id: 1528817077286,
+        //   completed: false
+        // },
+        // {
+        //   task: 'Bake Cookies',
+        //   id: 1528817084358,
+        //   completed: false
+        // }
       ],
-      newTask: ''
+      newTask: '',
+      searchText: ''
     };
   }
 
@@ -34,15 +36,23 @@ class App extends React.Component {
     let data = JSON.stringify(this.state.todoList);
     window.localStorage.setItem('react-todo-noble', data);
   }
-    const task = {
-      task: this.state.newTask,
-      id: Date.now(),
-      completed: false
-    };
-    this.setState(prevState => ({
-      todoList: [...prevState.todoList, task],
-      newTask: ''
-    }));
+
+  addTask = event => {
+    event.preventDefault();
+    
+    let newTask = this.state.newTask;
+
+    if (newTask.length !== 0) {
+      const task = {
+        task: this.state.newTask,
+        id: Date.now(),
+        completed: false
+      };
+      this.setState(prevState => ({
+        todoList: [...prevState.todoList, task],
+        newTask: ''
+      }));
+    }
   };
 
   inputHandler = event => {
@@ -51,8 +61,42 @@ class App extends React.Component {
 
   keyPressed = event => {
     if (event.key === 'Enter') {
-      this.addTask();
+      this.addTask(event);
     }
+  };
+
+  clearAll = () => {
+    this.setState({ todoList: [], newTask: '' });
+  };
+
+  clearCompleted = () => {
+    const uncompletedList = this.state.todoList.filter(
+      item => item.completed !== true
+    );
+    this.setState({ todoList: uncompletedList });
+  };
+
+  completedTodo = id => {
+    const todoIndex = this.state.todoList.findIndex(item => item.id === id);
+    const todoList = this.state.todoList;
+    todoList[todoIndex].completed = !todoList[todoIndex].completed;
+
+    this.setState({ todoList });
+  };
+
+  removeTodo = id => {
+    const todoList = this.state.todoList.filter(item => item.id !== id);
+    this.setState({ todoList });
+  };
+
+  searchTextInputHandler = event => {
+    this.setState({ searchText: event.target.value });
+  };
+
+  searchTodo = searchText => {
+    return todo => {
+      return todo.task.toLowerCase().includes(searchText.toLowerCase());
+    };
   };
 
   render() {
@@ -61,7 +105,19 @@ class App extends React.Component {
         <div className="bg-img" />
         <h2>Welcome to your Todo App!</h2>
         <div className="todo-container">
-          <TodoList todoList={this.state.todoList} />
+          <Search
+            inputChange={this.searchTextInputHandler}
+            text={this.state.searchText}
+          />
+          <TodoList
+            searchTerm={this.state.searchText}
+            searchTodo={this.searchTodo}
+            todoList={this.state.todoList}
+            clearAll={this.clearAll}
+            clearCompleted={this.clearCompleted}
+            completeTodo={this.completedTodo}
+            removeTodo={this.removeTodo}
+          />
           <TodoForm
             inputHandler={this.inputHandler}
             enterKeyHandler={this.keyPressed}
